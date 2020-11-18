7f426c88e72f2504255859deeb4ee47714573fd5
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-11-19 01:51:22 +0530

    [Kirit]Added:employee payroll data  class

--- js/employee_payroll_data.js
+++ js/employee_payroll_data.js
@@ -1,18 +1,7 @@
 class EmployeePayrollData{
-    name;
 
-
-   constructor(...params){
-        this.name = params[0];
-        this.picture = params[1];
-        this.gender = params[2];
-        this.department = params[3];
-        this.salary = params[4];
-        this.startDate = params[5];
-        this.note = params[6];
-    }
-
-  
+    get id(){return this._id;}
+    set id(id){this._id = id;}
 
     get name(){return this._name;}
     set name(name){
@@ -37,7 +26,6 @@ class EmployeePayrollData{
     set startDate(startDate){
         if(startDate <= new Date())this._startDate = startDate;
         else throw "Enter valid date";
-       this._startDate = startDate;
     }
 
     get note(){return this._note;}
@@ -47,6 +35,6 @@ class EmployeePayrollData{
         const options ={ year : "numeric", month : "long", day : "numeric"};
         const empDate = this.startDate === undefined ? "undefined" :
                        this.startDate.toLocaleDateString("en-US", options);
-       return "name = "+ this.name + " gender = "+ this.gender +" department = "+ this.department + " salary = "+ this.salary;
+       return "id= "+ this.id+" name = "+ this.name + " picture= "+ this.picture+" gender = "+ this.gender +" department = "+ this.department + " salary = "+ this.salary+ " startDate= "+ this.startDate+ " note= "+ this.note;
     }
 }
